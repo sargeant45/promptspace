1aa70b5683b9373faa7fb8c6e1c70069d51735ef
Ethan Arterberry <ethanarterberry@gmail.com>
2015-04-18 18:07:08 -0700

    add a shit ton of things
    - Gist support (hopefully)
    - Other things!

--- js/main.js
+++ js/main.js
@@ -11,8 +11,55 @@ function promptMe() {
           // extract the value
           var randomLine = fileContentLines[ randomLineIndex ];
           // add the random line in a 'p'
-          document.getElementsByName( 'prompt' )[0].innerHTML = randomLine;
-      };
-      request.open( 'GET', 'writingprompts.txt', true );
-      request.send();
+          document.getElementById('prompt').innerHTML = randomLine;
+  };
+  request.open( 'GET', 'writingprompts.txt', true );
+  request.send();
+  
+}
+
+function load() {
+  promptMe();
+  doGist();
+}
+
+function gist() {
+  localStorage.setItem("gist-content", document.getElementById("input").value);
+  localStorage.setItem("gist-desc", document.getElementById("prompt").innerHTML);
+  window.location.href = "https://github.com/login/oauth/authorize?client_id=ccd204482758a6a9b474&scope=gist";
+}
+
+function doGist() {
+  $.post( "https://github.com/login/oauth/access_token", {code: getUrlParameter("code")})
+  .done(function( data ) {
+    var access = data.access_token;
+  });
+  var github = new Github({
+    token: access,
+    auth: "oauth"
+  });
+  var gist = github.getGist();
+  var gistopts = {
+    "description": localStorage.getItem("gist-desc"),
+    "files": {
+      "prompt.md": {
+        "content": localStorage.getItem("gist-content"),
+      }
+    }
+  };
+  gist.update(gistopts);
+}
+
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
 }
